f135182fec53a9b74fc146f8d1a60f507bbff139
Vlad <vladman-08@mail.ru>
2020-03-10 12:24:35 +0200

    new version task
    new version task

---
+++ ConsoleAppThreadNew/ConsoleAppThreadNew/Class/JobExecutor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ConsoleAppThreadNew.Interface;
+
+namespace ConsoleAppThreadNew.Class
+{
+    class JobExecutor:IJobExecutor
+    {
+        private Queue<Action> _queueActions = new Queue<Action>();
+
+        private Thread[] _threads;
+        public int Amount { get; set; }
+
+        public JobExecutor()
+        {
+            Amount = 0;
+        }
+        
+        public void Start(int maxConcurrent)
+        {
+            var tempMaxConcurrent = 0;
+           
+            ThreadPool.SetMaxThreads(maxConcurrent, maxConcurrent);
+            ThreadPool.GetMaxThreads(out tempMaxConcurrent, out tempMaxConcurrent);
+            Console.WriteLine($"Установлено максимальное количество потоков: {tempMaxConcurrent}");
+
+            if (_queueActions.Any())
+            {
+                Amount = _queueActions.Count;
+                _threads = new Thread[_queueActions.Count];
+
+                for (int i = 0; i < _threads.Length; i++)
+                {
+                    var action = _queueActions.Dequeue();
+                    _threads[i] = new Thread(new ThreadStart(action)) { Name = $"Поток: {i}" };
+                    _threads[i].Start();
+                    Console.WriteLine($"Поток \"{ _threads[i].Name}\" обработан");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Очередь задач пуста!");
+            }
+        }
+
+        public void Stop()
+        {
+            int numberOfStopped = _threads.Length;
+
+            foreach (var thread in _threads)
+            {
+                thread.Abort();
+            }
+
+            Console.WriteLine($"Обработка остановлена.Количество остановленных потоков: {numberOfStopped}");
+        }
+
+        public void Add(Action action)
+        {
+           _queueActions.Enqueue(action);
+        }
+
+        public void Clear()
+        {
+            int numberOfCleared = _queueActions.Count;
+            _queueActions.Clear();
+            Console.WriteLine($"Очистка очереди. Количество задач: {numberOfCleared}");
+
+        }
+
+
+        //ожидание завершения потоков
+
+        public bool AwaitingFlow()
+        {
+            Console.WriteLine($"Ожидание завершения всех потоков...");
+            for (int i = 0; i < _threads.Length; i++)
+            {
+                _threads[i].Join();
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ ConsoleAppThreadNew/ConsoleAppThreadNew/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleAppThreadNew.Class;
+
+namespace ConsoleAppThreadNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JobExecutor je = new JobExecutor();
+            PersonSource ps = new PersonSource();
+            je.Add(ps.GetPerson);
+            je.Add(ps.GetPerson);
+            je.Add(ps.GetPerson);
+            je.Add(ps.GetPerson);
+            je.Start(4);
+            je.Add(ps.GetPerson);
+            je.Add(ps.GetPerson);
+            je.Stop();
+            je.Start(5);
+            je.Add(ps.GetPerson);
+            je.Add(ps.GetPerson);
+            je.AwaitingFlow();
+            je.Clear();
+            je.Stop();
+            
+            Console.ReadKey();
+        }
+        
+    }
+}
